4d0045b01719e4fa2a4dc9d373bab1d7c1080931
Mark Christian Tan <markcmtan@gmail.com>
2024-08-13 11:52:25 +0800

    feat: create context bridge for ipc events and expose it to frontend

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,10 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  startRecording: () => ipcRenderer.send("start-recording"),
+  stopRecording: () => ipcRenderer.send("stop-recording"),
+  replayActions: () => ipcRenderer.send("replay-actions"),
+});
