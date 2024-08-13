dcae29473a32c1497675c54a686ab544d6a5283c
Mark Christian Tan <markcmtan@gmail.com>
2024-08-13 11:25:03 +0800

    chore: configure tailwindcss in webpack.renderer.config and webpack.rules

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,8 +1,19 @@
-const rules = require('./webpack.rules');
+const rules = require("./webpack.rules");
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [
+    { loader: "style-loader" },
+    { loader: "css-loader" },
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: [require("tailwindcss"), require("autoprefixer")],
+        },
+      },
+    },
+  ],
 });
 
 module.exports = {
--- webpack.rules.js
+++ webpack.rules.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = [
   // Add support for native node modules
   {
@@ -25,6 +27,11 @@ module.exports = [
         presets: ["@babel/preset-react"],
       },
     },
+  },
+  {
+    test: /\.css$/,
+    include: [path.resolve(__dirname, "app/src")],
+    use: ["style-loader", "css-loader", "postcss-loader"],
   },
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
